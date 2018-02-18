58fbdfe05f5a8e38249071dfd24eb7d9c1babd5d
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-02-19 00:36:20 +0200

    added team routing to app.js

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const config = require('./config.json')
 
+const teamRouter = require('./routes/team')
+
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use('/api/team', teamRouter)
 
 app.listen(config.port, function () {
   console.log('started server on port ' + config.port)
