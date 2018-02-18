56604d39e8daebb05330b8553574ef650c2097ce
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-02-18 13:39:08 +0200

    made insertMatch check if team exists

--- bl/team-info-controller.js
+++ bl/team-info-controller.js
@@ -3,19 +3,15 @@ const dbController = require('../dal')
 function insertMatch (match) {
   return new Promise((resolve, reject) => {
     const teamNumber = match.teamNumber
-    dbController.matchExists(match.number, teamNumber)
+    dbController.teamExists(teamNumber)
       .then(exists => {
-        if (exists) {
-          const err = Error(`Match ${match.number} for team ${teamNumber} was already saved`)
-          err.name = 'match-already-saved'
-          reject(err)
-        } else {
-          dbController.insertMatch(match, teamNumber)
-            .then(() => resolve())
-            .catch(err => reject(err))
+        if (!exists) {
+          dbController.insertTeam({number: teamNumber, matches: {}})
         }
+        dbController.insertMatch(match, teamNumber)
+          .then(() => resolve)
+          .catch(err => reject(err))
       })
-      .catch(err => reject(err))
   })
 }
 
