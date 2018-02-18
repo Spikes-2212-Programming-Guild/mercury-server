8f091ce34a80f407ce41e413cf3f0932764c0fd5
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-02-19 00:38:32 +0200

    added game-config-router to app.js

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const bodyParser = require('body-parser')
 const config = require('./config.json')
 
 const teamRouter = require('./routes/team')
+const gameConfigRouter = require('./routes/game-config')
+
+const gameConfigManager = require('./dal/game-config-manager')
+gameConfigManager.load(config.gameName)
 
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/api/team', teamRouter)
+app.use('/api/game-config', gameConfigRouter)
 
 app.listen(config.port, function () {
   console.log('started server on port ' + config.port)
