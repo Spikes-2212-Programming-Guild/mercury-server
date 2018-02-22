4eb633d59cf1bc2cae3e9df5e6587c726412be42
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-02-22 07:06:50 +0000

    made matchExists async

--- bl/insert-match-controller.js
+++ bl/insert-match-controller.js
@@ -2,23 +2,30 @@ const dbController = require('../dal')
 
 function insertMatch (match) {
   return new Promise((resolve, reject) => {
+    function saveMatch () {
+      dbController.matchExists(match.matchnumber, teamNumber)
+        .then(matchExists => {
+          if (!matchExists) {
+            dbController.insertMatch(match, teamNumber)
+              .then(() => resolve())
+              .catch(err => reject(err))
+          } else {
+            reject(Error('Match Was Already Saved'))
+          }
+        })
+        .catch(err => reject(err))
+    }
     const teamNumber = match.teamnumber
     dbController.teamExists(teamNumber)
       .then(exists => {
         if (!exists) {
           dbController.insertTeam({number: teamNumber, matches: {}})
+            .then(() => saveMatch())
+        } else {
+          saveMatch()
         }
-        dbController.matchExists(match.matchnumber, teamNumber)
-          .then(matchExists => {
-            if (!matchExists) {
-              dbController.insertMatch(match, teamNumber)
-                .then(() => resolve())
-                .catch(err => reject(err))
-            } else {
-              reject(Error('Match Was Already Saved'))
-            }
-          })
       })
+      .catch(err => reject(err))
   })
 }
 
